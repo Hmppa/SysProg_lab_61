e5cb7f2d541058667a490640b72bc2fe0f793dc4
Hmppa <t5lojo00students.oamk.fi>
2018-10-16 23:39:34 +0300

    commit

--- app/src/main/java/co/example/johan_lohi/sysprog_lab_61/MainActivity.java
+++ app/src/main/java/co/example/johan_lohi/sysprog_lab_61/MainActivity.java
@@ -1,10 +1,13 @@
 package co.example.johan_lohi.sysprog_lab_61;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.textclassifier.TextLinks;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -18,44 +21,81 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class MainActivity extends ListActivity {
 
     ListView lv;
     TextView tv;
+    ArrayList<HashMap<String,String>> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv = (ListView) findViewById(R.id.listView);
+        arrayList = new ArrayList<>();
+        lv = getListView();
+
         tv = (TextView) findViewById(R.id.textView);
         RequestQueue queue = Volley.newRequestQueue(this);
 
 
-        String url = "https://www.amica.fi/api/restaurant/menu/day?date=2018-10-15&language=en&restaurantPageId=66287";
+        String url = "https://www.amica.fi/api/restaurant/menu/day?date=2018-10-16&language=en&restaurantPageId=66287";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        tv.setText(response.toString());
-                    }
-                   /*     try {
-                            JSONArray array = response.getJSONArray("LunchMenu");
+                        try {
 
-                            for (int i = 0; i < array.length(); i++)
+
+                            JSONObject lunchMenu = response.getJSONObject("LunchMenu");
+                            JSONArray setMenus = lunchMenu.getJSONArray("SetMenus");
+
+                            for (int i = 0; i < setMenus.length(); i++)
                             {
-                                JSONObject meal = array.getJSONObject(i);
+                                JSONObject menu = setMenus.getJSONObject(i);
+                                JSONArray meal = menu.getJSONArray("Meals");
 
-                                String name = meal.getString("SetMenus");
-
-                                tv.append(name + "\n");
+                                JSONObject mealfood = meal.getJSONObject(0);
+                                String meal1 = mealfood.getString("Name");
+                                String meal2 = "";
+                                if (!meal.isNull(1)) {
+                                JSONObject meal2Food = meal.getJSONObject(1);
+                                meal2 = meal2Food.getString("Name");
                             }
+                                HashMap<String,String> hashMap = new HashMap<>();
+                                hashMap.put("food1", meal1);
+                                hashMap.put("food2", meal2);
+                                arrayList.add(hashMap);
 
-                        } catch (JSONException e) {
+                               /* for (int j = 0; j < meal.length(); j++) {
+                                    JSONObject mealsJSONObject = meal.getJSONObject(j);
+
+                                    String name = mealsJSONObject.getString("Name");
+                                    tv.append(name + "\n");
+                                }*/
+
+
+                            }
+                            ListAdapter adapter = new SimpleAdapter(
+                                    MainActivity.this,
+                                    arrayList,
+                                    R.layout.listitems,
+                                    new String[] {"food1", "food2" },
+                                    new int[] {R.id.item1, R.id.item2}
+                            );
+                            lv.setAdapter(adapter);
+                            //String meal = response.get("SortOrder").toString();
+                            //tv.setText(jsonArray.get(0).toString());
+                            Log.d("json1", "try");
+
+                        } catch (Exception e) {
+                            Log.d("json1", "catch");
                             e.printStackTrace();
                         }
-
-                    }*/
+                    }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
@@ -63,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
                         error.printStackTrace();
                     }
                 });
-        //queue.add(jsonObjectRequest);
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
 
     }
